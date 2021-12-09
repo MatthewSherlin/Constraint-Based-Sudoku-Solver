2a17ed9a7212e3e56c90c9b870d4d6f570e0131f
Matthew Sherlin <mattssherlin@gmail.com>
2021-12-09 14:38:40 -0500

    fixed driver code

--- SudokuSolver.cs
+++ SudokuSolver.cs
@@ -7,22 +7,22 @@ namespace SudokuSolver
         static void Main(string[] args)
         {
             int[,] SudokuGrid = new int[,] {
-                { 0, 0, 0, 2, 6, 0, 7, 0, 1 },
-                { 6, 8, 0, 0, 7, 0, 0, 9, 0 },
-                { 1, 9, 0, 0, 0, 4, 5, 0, 0 },
-                { 8, 2, 0, 1, 0, 0, 0, 4, 0 },
-                { 0, 0, 4, 6, 0, 2, 9, 0, 0 },
-                { 0, 5, 0, 0, 0, 3, 0, 2, 8 },
-                { 0, 0, 9, 3, 0, 0, 0, 7, 4 },
-                { 0, 4, 0, 0, 5, 0, 0, 3, 6 },
-                { 7, 0, 3, 0, 1, 8, 0, 0, 0 }
+                {0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                {0, 0, 0, 6, 0, 0, 0, 0, 3}, 
+                {0, 7, 4, 0, 8, 0, 0, 0, 0}, 
+                {0, 0, 0, 0, 0, 3, 0, 0, 2}, 
+                {0, 8, 0, 0, 4, 0, 0, 1, 0}, 
+                {6, 0, 0, 5, 0, 0, 0, 0, 0}, 
+                {0, 0, 0, 0, 1, 0, 7, 8, 0}, 
+                {5, 0, 0, 0, 0, 9, 0, 0, 0}, 
+                {0, 0, 0, 0, 0, 0, 0, 4, 0}
             };
 
             if(SudokuGrid == null){
                 Console.WriteLine("Sudoku can not be solved.");
             }
             else{
-                if (SolveSudoku(SudokuGrid, 0, 0))
+                if (SolveSudoku(SudokuGrid, -1, -1))
             {
                 Console.WriteLine("Sudoku Board Completed.");
                 print_board(SudokuGrid);
@@ -35,54 +35,56 @@ namespace SudokuSolver
 
 
         //////////////////////////////////////////////////////////
-        //main solving driver checking all cases
-        public static bool SolveSudoku(int[,] sudoku, int row, int column)
+        //main solving driver with backtracking
+        public static bool SolveSudoku(int[,] sudoku, int row, int col)
         {
-            //if column becomes 9, move to next row and reset column
-            if (column == 9)
-            {
-                row += 1;
-                column = 0;
-                if (row == 9) return true;
-                
-            }
-            //size of rows and columns
-            if (sudoku[row, column] != 0) return SolveSudoku(sudoku, row, column + 1);
-            for (int i = 1; i < 10; i++)
-            {
-                //if constraints pass, index is equal to number in for loop
-                if (RowConstraint(sudoku, row, i) && ColConstraint(sudoku, column, i) && SubSquareConstraint(sudoku, row, column, i))
-                {
-                    //if constraints pass, then number added to index, and function ran again with new
-                    sudoku[row, column] = i;
-                    if (SolveSudoku(sudoku, row, column + 1)) return true;
-                }
-                else
-                {
-                    //if constraints don't pass, set index to 0 and go again with new number.
-                    sudoku[row, column] = 0;
+        bool filled = true;
+        // checking for each index if it is 0
+        for (int i = 0; i < 9; i++){
+            for (int j = 0; j < 9; j++){
+                //if index is 0, then set row and column to that index and filled is false
+                if (sudoku[i, j] == 0){
+                    row = i; col = j;
+                    filled = false;
+                    break;
                 }
             }
-            //if returns false, unable to solve sudoku board
-            return false;
+            if (!filled) break;
         }
+ 
+        // if there are no empty spaces left, then return true
+        if (filled) return true;
+
+        //size of rows and columns
+        for (int i = 1; i <= 9; i++){
+            //if constraints pass, index is equal to number in for loop
+            if (RowConstraint(sudoku, row, i) && ColConstraint(sudoku, col, i) && SubSquareConstraint(sudoku, row, col, i))
+            {
+                //if constraints pass, then number added to index
+                sudoku[row, col] = i;
+                if (SolveSudoku(sudoku, row, col)) return true;
+                //if constraints don't pass, set index to 0 and go again with new number.
+                else sudoku[row, col] = 0;
+            }
+        }
+        //if returns false, unable to solve sudoku board
+        return false;
+    }
 
         ////////////////////////////////////////////////////////////////////////////
         //contraints
-        public static bool RowConstraint(int[,] sudoku, int row, int number)
-        {
+        public static bool RowConstraint(int[,] sudoku, int row, int number){
             //for length of row, check each index for number
-            for (int i = 0; i < 8; ++i)
+            for (int i = 0; i <= 8; ++i)
             {
                 if (number == sudoku[row, i]) return false;
             }
             return true;
         }
 
-        public static bool ColConstraint(int[,] sudoku, int column, int number)
-        {
+        public static bool ColConstraint(int[,] sudoku, int column, int number){
             //for length of row, check each index for number
-            for (int i = 0; i < 8; ++i)
+            for (int i = 0; i <= 8; ++i)
             {
                 if (number == sudoku[i, column]) return false;
             }
@@ -92,24 +94,19 @@ namespace SudokuSolver
         ////////////////////
         /////needs fixing
         ///////////////////
-        public static bool SubSquareConstraint(int[,] sudoku, int row, int column, int num)
-        {
-            for (int i = 0; i < 3; ++i)
-            {
-                for (int j = 0; j < 3; ++j)
-                {
-                    if (num == sudoku[i+(row-row%3), j+(column-column%3)])
-                        return false;
+        public static bool SubSquareConstraint(int[,] sudoku, int row, int column, int num){
+            for (int i = 0; i < 3; i++){
+                for (int j = 0; j < 3; j++){
+                    if (num == sudoku[i + (row-row%3), j + (column-column%3)]) return false;
                 }
             }
-            return true;
+        return true;
         }
 
         //
         ////////////////////////////////////////////////////////////////////////////
 
-        public static void print_board(int[,] board)
-        {
+        public static void print_board(int[,] board){
             for (int i = 0; i < 9; ++i)
             {
                 if (i % 3 == 0 && i != 0)
@@ -131,5 +128,4 @@ namespace SudokuSolver
             }
         }
     }
-
 }
