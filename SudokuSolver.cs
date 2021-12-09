278cfe698beccb5bd0340da50fe194344392658e
Matthew Sherlin <mattssherlin@gmail.com>
2021-12-09 15:09:53 -0500

    quick cosmetic issues

--- SudokuSolver.cs
+++ SudokuSolver.cs
@@ -36,7 +36,7 @@ namespace SudokuSolver
 
         //////////////////////////////////////////////////////////
         //main solving driver with backtracking
-        public static bool SolveSudoku(int[,] sudoku, int row, int col)
+        public static bool SolveSudoku(int[,] sudoku, int row, int column)
         {
         bool filled = true;
         // checking for each index if it is 0
@@ -44,7 +44,7 @@ namespace SudokuSolver
             for (int j = 0; j < 9; j++){
                 //if index is 0, then set row and column to that index and filled is false
                 if (sudoku[i, j] == 0){
-                    row = i; col = j;
+                    column = j; row = i;
                     filled = false;
                     break;
                 }
@@ -56,18 +56,20 @@ namespace SudokuSolver
         //size of rows and columns
         for (int i = 1; i <= 9; i++){
             //if constraints pass, index is equal to number in for loop
-            if (RowConstraint(sudoku, row, i) && ColConstraint(sudoku, col, i) && SubSquareConstraint(sudoku, row, col, i))
+            if (RowConstraint(sudoku, row, i) && ColConstraint(sudoku, column, i) && SubSquareConstraint(sudoku, row, column, i))
             {
                 //if constraints pass, then number added to index
-                sudoku[row, col] = i;
-                if (SolveSudoku(sudoku, row, col)) return true;
+                sudoku[row, column] = i;
+                if (SolveSudoku(sudoku, row, column)) return true;
                 //if constraints don't pass, set index to 0 and go again with new number.
-                else sudoku[row, col] = 0;
+                else sudoku[row, column] = 0;
             }
         }
         //if returns false, unable to solve sudoku board
         return false;
     }
+
+
 
         ////////////////////////////////////////////////////////////////////////////
         //contraints
@@ -89,9 +91,6 @@ namespace SudokuSolver
             return true;
         }
 
-        ////////////////////
-        /////needs fixing
-        ///////////////////
         public static bool SubSquareConstraint(int[,] sudoku, int row, int column, int num){
             for (int i = 0; i < 3; i++){
                 for (int j = 0; j < 3; j++){
@@ -104,6 +103,8 @@ namespace SudokuSolver
         //
         ////////////////////////////////////////////////////////////////////////////
 
+        ////////////////////////////////////////////////////////////////////////////
+        //print board
         public static void print_board(int[,] board){
             for (int i = 0; i < 9; ++i)
             {
